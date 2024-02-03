c3a4faa26776b94622624d69d3edd4881e47a11d
Mia Nguyen <quynhnhu11579@gmail.com>
2024-02-03 17:48:09 -0330

    Update shadebound page

--- app/shadebound/page.tsx
+++ app/shadebound/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Image from 'next/image'
 import { Shadebound } from "@/constants";
 import React from "react";
 import { slideInFromTop, slideInFromBottom, slideInFromLeft } from '@/utils/motion'
@@ -52,50 +53,76 @@ export default function genrify({ }: Props) {
           ))}
         </div>
 
-        <motion.p
+        <motion.div
           initial="hidden"
           animate='visible'
           variants={slideInFromLeft(0.75)}
           className='text-white text-1xl md:text-1xl my-8 max-w-[1000px] indent-8'>
-          This project contains three levels that players can move through and can revisit the level once completed. Each level offers a unique map, with mobs varying from level to level,
-          with the final boss, the dragon, met at the end of the final level. To keep things interesting and challenging yourself, we implemented a difficulty setting,
-          for novice players to understand and ease into the game and keep gaming enthusiasts challenged and engaged. Players can create their own challenges as well.
-          With the Level Editor, players can customize each level to be as tame or wild as they wish, never leaving a dull moment.
-          <br />
-          <img src="/shadebound1.png" alt="" />
-        </motion.p>
+          <p className='mb-8'>
+            This project contains three levels that players can move through and can revisit the level once completed. Each level offers a unique map, with mobs varying from level to level,
+            with the final boss, the dragon, met at the end of the final level. To keep things interesting and challenging yourself, we implemented a difficulty setting,
+            for novice players to understand and ease into the game and keep gaming enthusiasts challenged and engaged. Players can create their own challenges as well.
+            With the Level Editor, players can customize each level to be as tame or wild as they wish, never leaving a dull moment.
+          </p>
+          <Image
+            src={"/shadebound1.png"}
+            alt={"Shadebound 1"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%' }}
+          />
+        </motion.div>
+
+        <motion.div
+          initial="hidden"
+          animate='visible'
+          variants={slideInFromLeft(0.75)}
+          className='text-white text-1xl md:text-1xl mb-8 max-w-[1000px] indent-8'>
+          <p className='mb-8'>
+            You as the hero have multiple methods and ways you can complete your journey and defeat the final boss. In your possession are two weapons; a golden sword which you can use
+            to directly attack your enemies, facing them in one-on-one combat, or the golden bow, which you can use to attack at a distance, while the ammo lasts. But beware: some enemies
+            are sharper than they seem, and will follow you seeking revenge. But you can be faster. Dashing, wall-jumping and double-jumping, you can parkour through each platform and right
+            above enemies' shoulders and swiftly take them down.
+          </p>
+          <Image
+            src={"/shadebound_gameover.png"}
+            alt={"Shadebound Game Over"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%' }}
+          />
+        </motion.div>
+
+        <motion.div
+          initial="hidden"
+          animate='visible'
+          variants={slideInFromLeft(0.75)}
+          className='text-white text-1xl md:text-1xl mb-8 max-w-[1000px] indent-8'>
+          <p className='mb-8'>
+            Say you take an arrow to the knee, notching down your health and making you one step closer to &apos;Game Over&apos;, use the coins you collected throughout the maps to purchase items and
+            potions through the Shadebound Shop, and keep them for when you need. You can stock up on health packs and regain your HP, a sprint potion to boost your speed despite the weak
+            knees, and plot out your revenge with the full map scroll, which allows you to see where all those who oppose you loiter, even beyond the mini-map of the local area. Once you are
+            faced with the mobs, you can use a potion of slowness to make them sluggish to your attacks, or you can leave them confused, blind and unable to attack with the smoke bomb. If
+            you feel particularly vengeful, you can fill up on the Quad-Damage potion to bludgeon your enemies to dust. All these tools and potions are locally sourced and conveniently
+            available at anytime right at the Shadebound Shop.
+          </p>
+          <Image
+            src={"/shadebound2.png"}
+            alt={"Shadebound 2"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%' }}
+          />
+        </motion.div>
 
         <motion.p
           initial="hidden"
           animate='visible'
           variants={slideInFromLeft(0.75)}
-          className='text-white text-1xl md:text-1xl my-8 max-w-[1000px] indent-8'>
-          You as the hero have multiple methods and ways you can complete your journey and defeat the final boss. In your possession are two weapons; a golden sword which you can use
-          to directly attack your enemies, facing them in one-on-one combat, or the golden bow, which you can use to attack at a distance, while the ammo lasts. But beware: some enemies
-          are sharper than they seem, and will follow you seeking revenge. But you can be faster. Dashing, wall-jumping and double-jumping, you can parkour through each platform and right
-          above enemies' shoulders and swiftly take them down.
-          <br />
-          <img src="/shadebound_gameover.png" alt="" />
-        </motion.p>
-
-        <motion.p
-          initial="hidden"
-          animate='visible'
-          variants={slideInFromLeft(0.75)}
-          className='text-white text-1xl md:text-1xl my-8 max-w-[1000px] indent-8'>
-          Say you take an arrow to the knee, notching down your health and making you one step closer to "Game Over", use the coins you collected throughout the maps to purchase items and
-          potions through the Shadebound Shop, and keep them for when you need. You can stock up on health packs and regain your HP, a sprint potion to boost your speed despite the weak
-          knees, and plot out your revenge with the full map scroll, which allows you to see where all those who oppose you loiter, even beyond the mini-map of the local area. Once you are
-          faced with the mobs, you can use a potion of slowness to make them sluggish to your attacks, or you can leave them confused, blind and unable to attack with the smoke bomb. If
-          you feel particularly vengeful, you can fill up on the Quad-Damage potion to bludgeon your enemies to dust. All these tools and potions are locally sourced and conveniently
-          available at anytime right at the Shadebound Shop.
-        </motion.p>
-
-        <motion.p
-          initial="hidden"
-          animate='visible'
-          variants={slideInFromLeft(0.75)}
-          className='text-white text-1xl md:text-1xl my-8 max-w-[1000px] indent-8'>
+          className='text-white text-1xl md:text-1xl mb-8 max-w-[1000px] indent-8'>
           Customize and play your way. Be the hero you were meant to be, in Shadebound.
         </motion.p>
       </div>
